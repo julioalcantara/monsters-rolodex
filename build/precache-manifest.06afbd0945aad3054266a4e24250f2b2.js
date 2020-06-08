1849342eb2e2d0e78b6032d961e03dbca96caadb
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-09 00:49:39 +0100

    adding files for github pages

---
+++ build/precache-manifest.06afbd0945aad3054266a4e24250f2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a245825b2ea6dd7ae27bfab11b262d7",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "55ad744435dd727b6d45",
+    "url": "/mosters-rolodex/static/css/main.52d073a9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ad744435dd727b6d45",
+    "url": "/mosters-rolodex/static/js/main.58a4784c.chunk.js"
+  },
+  {
+    "revision": "3b1d11c5c06fc5abea79",
+    "url": "/mosters-rolodex/static/js/runtime-main.f637eaeb.js"
+  }
+]);
